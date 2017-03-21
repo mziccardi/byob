63386cf3cde5ab5834a154a6ae34717c53259c2d
Mike Ziccardi <mikeziccardi@gmail.com>
2017-03-21 17:58:22 -0600

    seed files are running

--- db/migrations/20170321130341_first.js
+++ db/migrations/20170321130341_first.js
@@ -2,30 +2,28 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('restaurants',function(table){
-      table.increments('id').primary()
-      table.string('name')
-      table.string('type')
-
-
+      table.increments('id').primary();
+      table.string('name');
+      table.string('type');
       table.timestamps()
     }),
 
     knex.schema.createTable('reviews', function(table){
-      table.increments('id').primary()
+      table.increments('id').primary();
       table.integer('restaurantId')
         .references('id')
-        .inTable('restaurants')
+        .inTable('restaurants');
       table.integer('userId')
         .references('id')
-        .inTable('users')
-      table.text('review')
-      table.timestamps()
+        .inTable('users');
+      table.text('review');
+      table.timestamps();
     }),
 
     knex.schema.createTable('users',function(table){
       table.increments('id').primary()
-      table.string('firstname')
-      table.string('lastname')
+      table.string('firstname');
+      table.string('lastname');
 
       table.timestamps()
     })
