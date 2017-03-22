35604926eda7e43b5a4fe4c7dd7855f0c62de6be
Mike Ziccardi <mikeziccardi@gmail.com>
2017-03-22 00:37:17 -0600

    get requests respond with json data

--- server.js
+++ server.js
@@ -4,18 +4,43 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'BYOB'
 
 
 app.get('/api/restaurants', (request, response) => {
   database('restaurants').select()
-          .then((restaurants) => {
-            response.status(200).json(restaurants);
-          })
-          .catch((error) => {
-            console.error('something is wrong with the db');
-          })
+    .then((restaurants) => {
+      response.status(200).json(restaurants);
+    })
+    .catch((error) => {
+      console.error('something is wrong with the db');
+    })
+})
+
+app.get('/api/users',(request, response) =>{
+  database('users').select()
+    .then((users) =>{
+      response.status(200).json(users)
+    })
+    .catch((error)=>{
+      response.status(400)
+      console.error('USERS DONT WORK')
+    })
+})
+app.get('/api/reviews',(request, response) =>{
+  database('reviews').select()
+    .then((reviews) =>{
+      response.status(200).json(reviews)
+    })
+    .catch((error)=>{
+      response.status(400)
+      console.error('REVIEWS DONT WORK')
+    })
 })
 
 
