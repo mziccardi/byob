15a1be538afd0f03847ef712e75150923ce44894
Mike Ziccardi <mikeziccardi@gmail.com>
2017-03-25 13:20:57 -0600

    post request started.

--- server.js
+++ server.js
@@ -43,6 +43,33 @@ app.get('/api/reviews',(request, response) =>{
     })
 })
 
+app.get('/api/restaurants/:id/reviews', (request,response) => {
+  database('reviews').where('restaurantId', request.params.id)
+    .then((reviews) =>{
+      response.status(200).json(reviews)
+    })
+    .catch((error) =>{
+      console.error('cant find reviews for this restaurant')
+    })
+})
+
+app.post('/api/restaurants', (request, response)=>{
+  const { name, type } = request.body
+  const restaurant = { name, type};
+
+  database('restaurants').insert(restaurant)
+    .then(()=>{
+      database('restaurants').select()
+        .then((restaurants) =>{
+          response.status(200).json(restaurants)
+        })
+        .catch((error) => {
+          response.status(500)
+            console.error('POST RESTAURANTS IS BROKEN')
+        })
+    })
+})
+
 
 app.listen(app.get('port'), ()=>{
   console.log(`${app.locals.title} is running at ${app.get('port')}`)
